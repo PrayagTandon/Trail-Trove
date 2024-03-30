e11826f76baa51eabce6475423faad5d0d9b95cd
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-30 12:20:21 +0530

    Editing and Updating the campground

--- app.js
+++ app.js
@@ -16,16 +16,28 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }))
+
+/* The order in which routes are defined matters. Thus, the campgrounds/new route cannot come after campgrounds/:id */
+
 app.get('/', (req, res) => {
     res.render('home');
 });
-
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find();
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
