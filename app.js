14360ec107b88eff0278209b0f3afb3e76910d9a
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-30 10:46:58 +0530

    ...

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/campgrunds', async (req, res) => {
+app.get('/campgrounds', async (req, res) => {
     const campgrunds = await Campground.find();
     res.render('campgrounds/index');
 });
