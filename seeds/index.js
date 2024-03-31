5919576671baf38caf24eb3470cff3f8f4411933
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-31 23:17:34 +0530

    ...

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
@@ -13,17 +14,39 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+async function seedImg() {
+    try {
+        const resp = await axios.get('https://api.unsplash.com/photos/random', {
+            params: {
+                client_id: 'yIS7jL7t78s5g2m_8Ihl4LaMoEuTwn1xmJHTIJEG-kI',
+                collections: 11536948,
+            },
         })
-        await camp.save();
+        return resp.data.urls.small
+    } catch (err) {
+        console.error(err)
     }
 }
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 30; i++) {
+        // setup
+        const placeSeed = Math.floor(Math.random() * places.length)
+        const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
+        const citySeed = Math.floor(Math.random() * cities.length)
+
+        // seed data into campground
+        const camp = new Campground({
+            imageUrl: await seedImg(),
+            title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+            location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+            description:
+                'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+        })
+        await camp.save();
+    };
+};
 
 seedDB().then(() => {
     mongoose.connection.close();
